28da4c1d39fa3e2aa829cd7f697141427a22c00b
Ujjwaleshwar <banndband@gmail.com>
2022-06-21 21:03:25 +0530

    Added initial .py file

---
+++ rl.py
@@ -0,0 +1,21 @@
+import os
+import gym
+from stable_baselines3 import ppo
+from stable_baselines3.common.vec_env import DummyVecEnv
+from stable_baselines3.common.evaluation import evaluate_policy
+
+env = gym.make('CartPole-v0')
+
+episodes = 5
+for episode in range(1, episodes + 1):
+    state = env.reset()
+    done = False
+    score = 0
+
+    while not done:
+        env.render()
+        action = env.action_space.sample()
+        n_state, reward, done, info = env.step(action)
+        score += reward
+    print("Episode:{}, Score:{}".format(episode, score))
+env.close()

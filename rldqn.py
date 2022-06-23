83ea0862bea187d00e8563825572bc6b1b951aaa
Ujjwaleshwar <banndband@gmail.com>
2022-06-23 21:38:41 +0530

    Added DQN learning

--- rldqn.py
+++ rldqn.py
@@ -1,6 +1,6 @@
 import os
 import gym
-from stable_baselines3 import PPO, ppo
+from stable_baselines3 import DQN, PPO, ppo
 from stable_baselines3.common.vec_env import DummyVecEnv
 from stable_baselines3.common.evaluation import evaluate_policy
 from stable_baselines3.common.callbacks import EvalCallback, StopTrainingOnRewardThreshold
@@ -27,27 +27,22 @@ def test(env):
 
 def train(env):
     DummyVecEnv([lambda: env])
-    model = PPO('MlpPolicy', env, verbose=1, tensorboard_log=log_path)
-    model.learn(total_timesteps=20000)
-    return model
-
-def train(env, eval_callback):
-    DummyVecEnv([lambda: env])
-    model = PPO('MlpPolicy', env, verbose=1, tensorboard_log=log_path)
-    model.learn(total_timesteps=20000, callback = eval_callback)
+    model = DQN('MlpPolicy', env, verbose=1, tensorboard_log=log_path)
+    model.learn(total_timesteps=200000)
     return model
 
 def load(env, path):
-    model = PPO('MlpPolicy', env, verbose=1, tensorboard_log=log_path)
+    model = DQN('MlpPolicy', env, verbose=1, tensorboard_log=log_path)
+    model = DQN.load(os.path.join(save_path, path))
     # model.load(os.path.join(save_path, path), env=env) # Not working for some reason
-    model.set_parameters(os.path.join(save_path, path))
+    # model.set_parameters(os.path.join(save_path, path))
     return model
 
-stop_callback = StopTrainingOnRewardThreshold(reward_threshold=200, verbose=1)
-eval_callback = EvalCallback(env, callback_on_new_best=stop_callback, eval_freq=10000, best_model_save_path=os.path.join(save_path, 'PPO_cartpole_best'), verbose=1)
-model = train(env, eval_callback)
-# model.save(os.path.join(save_path, 'PPO_cartpole'));
-# model = load(env, 'PPO_cartpole')
-# result = evaluate_policy(model, env, n_eval_episodes=10, render=True)
-# print(result)
+# stop_callback = StopTrainingOnRewardThreshold(reward_threshold=200, verbose=1)
+# eval_callback = EvalCallback(env, callback_on_new_best=stop_callback, eval_freq=10000, best_model_save_path=os.path.join(save_path, 'PPO_cartpole_best'), verbose=1)
+# model = train(env)
+# model.save(os.path.join(save_path, 'DQN_cartpole'));
+model = load(env, 'DQN_cartpole')
+result = evaluate_policy(model, env, n_eval_episodes=10, render=True)
+print(result)
 test(env)
